0a7c610e129f45c95a5142d8684b43a1183aeeda
Akash Mitra <akashmitra@gmail.com>
2020-05-24 16:09:56 +0800

    boilerplate code

---
+++ src/hello-world.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        :class="textColorClass"
+    >
+        Hello {{name}}
+    </div>
+</template>
+
+<script>
+export default {
+
+    name: 'hello-world',
+
+    props: {
+        textColorClass: {
+            type: String,
+            default: 'text-blue-500'
+        }
+    },
+
+    data() {
+        return {
+            name: 'World',
+        }
+    },
+
+    methods: {}
+
+}
+</script>
